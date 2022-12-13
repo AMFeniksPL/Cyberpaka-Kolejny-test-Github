1d038f2240e917bf742df45bc35e5971a035cdef
Arkadiusz Mazurek <66950904+AMFeniksPL@users.noreply.github.com>
2022-12-13 18:49:14 +0100

    Podział na Frame i Label

--- testowyPliczek.py
+++ testowyPliczek.py
@@ -1,4 +1,14 @@
 from tkinter import *
 
 window = Tk()
+window.minsize(640, 480)
+window.minsize(640, 480)
+
+label1 = Frame(window, width = 640, height = 400, bd=3, bg="black")
+label2 = Label(window, text="Jakiś randomowy tekst", font = ("Arial", 24), bd=3, bg="green")
+
+label1.pack()
+label2.pack()
+
+
 window.mainloop()
