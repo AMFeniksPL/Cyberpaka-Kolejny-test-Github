08cbb0652ff79a19fab73de671e0e68d87122b4e
Arkadiusz Mazurek <66950904+AMFeniksPL@users.noreply.github.com>
2022-12-13 19:06:06 +0100

    Dodanie obrazka

--- testowyPliczek.py
+++ testowyPliczek.py
@@ -3,12 +3,19 @@ from tkinter import *
 window = Tk()
 window.minsize(640, 480)
 window.minsize(640, 480)
+window.geometry("640x480")
 
-label1 = Frame(window, width = 640, height = 400, bd=3, bg="black")
-label2 = Label(window, text="Jakiś randomowy tekst", font = ("Arial", 24), bd=3, bg="green")
+photo = PhotoImage(file="Zrzut ekranu 2022-12-11 193101.png")
 
-label1.pack()
-label2.pack()
+mainFrame = Label(window,
+                  text="Moja pierwsza gra (od dawna)",
+                  font = ("Arial", 28),
+                  bd=10,
+                  bg="black",
+                  fg="white",
+                  image=photo,
+                  compound="top")
 
+mainFrame.pack()
 
 window.mainloop()
